bef80d01cfc9143339ef0a7d0f925cd6fd113da0
Dhanoop Karunakaran <dhanoop.karunakaran@gmail.com>
2017-10-24 22:34:17 +1100

    changes

--- model.py
+++ model.py
@@ -1,6 +1,45 @@
 from keras.models import Sequential, Model 
 from keras.layers import Lambda, Cropping2D, Convolution2D, ELU, Flatten, Dense
+import os
+import csv
+from sklearn.model_selection import train_test_split
+import cv2
+import numpy as np
+import sklearn
 
+
+samples = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+train_generator = generator(train_samples, batch_size=32)
+
+print(train_generator)
 
 def nvidia_architecture():
     model = Sequential()
@@ -38,6 +77,5 @@ def nvidia_architecture():
 
     return model
 
-nvidia_architecture()
     
 
